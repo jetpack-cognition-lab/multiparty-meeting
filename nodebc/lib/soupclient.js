5ae6f5f10ca8720a23eeaf29efaa6307dc1910b6
Andi Pieper <andi@wowza.com>
2020-06-28 21:54:58 +0200

    added soupclient class

---
+++ nodebc/lib/soupclient.js
@@ -0,0 +1,374 @@
+const socketio = require('socket.io-client')
+const faker = require('faker')
+const execa = require('execa')
+const { EventEmitter } = require('events')
+
+//const gst = require('node-gstreamer-launch')
+
+class SoupClient extends EventEmitter {
+  constructor(
+    url, room, name
+  ) {
+    super()
+    this.name = name
+    this.url = url
+    this.roomId = room
+
+    this.routerRtpCapabilities = undefined
+    this.joinresp = undefined
+    this.transportOpts = undefined
+    this.gstProcess = undefined
+    this.playing = false
+    this.peerId = (Math.random() +1).toString(36).substr(2, 7)
+    this.joined = false
+
+    // socketio client
+    this.client = undefined
+
+    process.on('SIGINT', async function () {
+      console.log("CONTWOL ZEE WAS PWESSED")
+      try {
+        await sendRequest('closeProducer', { producerId: this.transportOpts.audioProducer.id })
+        await sendRequest('closeProducer', { producerId: this.transportOpts.videoProducer.id })
+      } catch(error) {}
+      // do some cleanup here?
+      process.exit(0)
+    })
+  }
+
+  async connect() {
+    ////////////////////////////////////////
+    this.client = socketio(`${this.url}?peerId=${this.peerId}peerId&roomId=${this.roomId}`)
+    // this.client = socketio(`https://soup.jetpack.cl:5443?peerId=${peerId}peerId&roomId=${roomId}`)
+    // const client = socketio(`https://space.miniclub.space:3443?peerId=${peerId}peerId&roomId=${roomId}`)
+
+    this.client.on('connect', function () {
+      console.log("connected")
+      this.emit('connected')
+    })
+
+    this.client.on('event', function (data) {
+      console.log("got event", data)
+      this.emit('event', data)
+    })
+
+    this.client.on('notification', this.notificationHandler.bind(this))
+
+    this.client.on('disconnect', function () {
+      console.log("disconnect")
+      this.joined = false
+      this.emit('disconnect')
+    })
+  }
+
+  async notificationHandler(notification) {
+    try {
+      switch (notification.method) {
+        case 'roomReady':
+        {
+          console.log("roomReady received")
+          // join room
+          await this.joinRoom()
+          this.joined = true
+          this.emit(notification.method, notification.data)
+          break;
+        }
+        default:
+        {
+          console.log("notification received", notification)
+          this.emit(notification.method, notification.data)
+          break;
+        }
+      }
+    } catch (error) {
+      console.error('error on socket "notification" event failed: "', error);
+      await sendChatMessage(`Shit: Error ${JSON.stringify(error)}`)
+    }  
+  }
+
+  async stopCurrentTrack() {
+    // console.log(state.gstProcess)
+    console.log("stop track")
+    await this.stopProducers()      
+
+    // await sendRequest('pauseProducer', { producerId: state.transportOpts.audioProducer.id })
+    // await sendRequest('pauseProducer', { producerId: state.transportOpts.videoProducer.id })
+    if (this.gstProcess) {
+      this.gstProcess.kill('SIGTERM', {
+        forceKillAfterTimeout: 2000
+      })
+    }
+    this.gstProcess = null
+    this.playing = false
+  }
+
+  async sendChatMessage(message) {
+    await this.sendRequest(
+      'chatMessage',
+      {
+        chatMessage: {
+          type: "message",
+          text: message,
+          time: new Date().valueOf(),
+          name: displayName,
+          sender: 'response',
+          picture: null
+        }
+      }
+    )
+  }
+
+  async execGstCommand(command) {
+    try {
+      const result = execa.command(command, {shell: false, env: {'GST_DEBUG': '2'}})
+      result.on('exit', () => {
+        console.log("exited", result)
+        this.stopProducers()
+        this.gstProcess = null
+        this.playing = false
+      })
+      this.gstProcess = result
+      this.playing = true
+    } catch (error) {
+      this.playing = false
+      //await stopProducers()      
+      console.log("catched error in execGstCommand: ", error)
+    }
+  }
+
+  async stopProducers() {
+    await this.sendRequest('closeProducer', {producerId: this.transportOpts.audioProducer.id })
+    await this.sendRequest('closeProducer', {producerId: this.transportOpts.videoProducer.id })
+    await this.sendRequest('closeTransport', {transportId: this.transportOpts.audioTransportId })
+    await this.sendRequest('closeTransport', {transportId: this.transportOpts.videoTransportId })
+  }
+
+  async startProducers() {
+    console.log("startproducers", this.transportOpts)
+
+    // create a transport for audio
+    console.log("createPlainTransport")
+    const audioTransportInfo = await this.sendRequest(
+      'createPlainTransport',
+      {
+        producing: true,
+        consuming: false
+      });
+
+    const audioTransportId = audioTransportInfo.id
+    const audioTransportIp = audioTransportInfo.ip
+    const audioTransportPort = audioTransportInfo.port
+    const audioTransportRtcpPort = audioTransportInfo.rtcpPort
+
+    console.log("audio transportInfo:", audioTransportInfo)
+
+    // create a transport for video
+    console.log("createPlainTransport")
+    const videoTransportInfo = await this.sendRequest(
+      'createPlainTransport',
+      {
+        producing: true,
+        consuming: false
+      });
+
+    const videoTransportId = videoTransportInfo.id
+    const videoTransportIp = videoTransportInfo.ip
+    const videoTransportPort = videoTransportInfo.port
+    const videoTransportRtcpPort = videoTransportInfo.rtcpPort
+
+    console.log("video transportInfo:", videoTransportInfo)
+
+    // produce
+    const audioProducer = await this.sendRequest(
+      'produce',
+      {
+        transportId: audioTransportId,
+        kind: 'audio',
+        appData: {
+          source: 'mic'
+        },
+        rtpParameters: {
+          encodings: [
+            {
+              ssrc: 1111
+            }
+          ],
+          codecs: [
+            {
+              name: "Opus",
+              mimeType: "audio/opus",
+              payloadType: 100, // "dynamic type" in rtp
+              channels: 2,
+              clockRate: 48000,
+              rtcpFeedback: [
+                {
+                  type: 'nack'
+                }
+              ],
+              parameters: {
+                useinbandfec: 1,
+                "sprop-stereo": 1
+              },
+            }
+          ]
+        }
+      }
+    )
+    console.log("audioproducer: ", audioProducer)
+    // await this.sendRequest('pauseProducer', { producerId: audioProducer.id })
+
+    const videoProducer = await this.sendRequest(
+      'produce',
+      {
+        transportId: videoTransportId,
+        kind: 'video',
+        appData: {
+          source: 'webcam'
+        },
+
+        rtpParameters: {
+          codecs: [
+            {
+              name: "VP8",
+              mimeType: "video/VP8",
+              payloadType: 101, // "dynamic type" in rtp
+              clockRate: 90000,
+              rtcpFeedback: [
+                { type: 'nack' },
+                { type: 'nack', parameter: 'pli' },
+                { type: 'ccm', parameter: 'fir' },
+              ]
+            }
+          ],
+          encodings: [
+            {
+              ssrc: 2222
+            }
+          ]
+        }
+      }
+    )
+    console.log("videoproducer: ", videoProducer)
+    // await this.sendRequest('pauseProducer', {producerId: videoProducer.id })
+    this.transportOpts = {
+      videoTransportId,
+      videoTransportIp,
+      videoTransportPort,
+      videoTransportRtcpPort,
+      videoPt: 101,
+      videoSSRC: 2222,
+      videoProducer,
+      audioTransportId,
+      audioTransportIp,
+      audioTransportPort,
+      audioTransportRtcpPort,
+      audioPt: 100,
+      audioSSRC: 1111,
+      audioProducer
+    }
+  }
+
+  async joinRoom() {
+    this.routerRtpCapabilities = await this.sendRequest('getRouterRtpCapabilities')
+    this.joinresp = await this.sendRequest(
+      'join',
+      {
+        displayName: this.name,
+        rtpCapabilities: {
+          codecs:
+            [{
+              'mimeType': 'audio/opus',
+              'clockRate': 48000,
+              'kind': 'audio',
+              'preferredPayloadType': 100,
+              'channels': 2,
+              'parameters': { 'useinbandfec': 1 },
+              'rtcpFeedback': []
+            },
+            {
+              'mimeType': 'video/VP8',
+              'clockRate': 90000,
+              'kind': 'video',
+              'preferredPayloadType': 101,
+              'parameters': {},
+              'rtcpFeedback': [{ 'type': 'nack' }]
+            },
+            {
+              'mimeType': 'video/VP9',
+              'clockRate': 90000,
+              'kind': 'video',
+              'preferredPayloadType': 103,
+              'parameters': {},
+              'rtcpFeedback': [{ 'type': 'nack' }]
+            },
+            {
+              'mimeType': 'video/H264',
+              'clockRate': 90000,
+              'kind': 'video',
+              'preferredPayloadType': 107,
+              'parameters': { 'packetization-mode': 1, 'profile-level-id': '42e01f', 'level-asymmetry-allowed': 1 },
+              'rtcpFeedback': [{ 'type': 'nack' }]
+            },
+            {
+              'mimeType': 'video/H265',
+              'clockRate': 90000,
+              'kind': 'video',
+              'preferredPayloadType': 109,
+              'parameters': {},
+              'rtcpFeedback': [{ 'type': 'nack' }]
+            }
+            ],
+          headerExtensions:
+            []
+        }
+      }
+    )
+    console.log(this.joinresp)
+  }
+
+  timeoutCallback(callback) {
+    let called = false
+
+    const interval = setTimeout(
+      () => {
+        if (called)
+          return;
+        called = true;
+        callback(new Error('Request timeout.'));
+      },
+      5000
+    );
+
+    return (...args) => {
+      if (called)
+        return;
+      called = true;
+      clearTimeout(interval)
+      callback(...args)
+    }
+  }
+
+  async sendRequest(method, data) {
+    return new Promise((resolve, reject) => {
+      if (!this.client) {
+        reject('No socket connection.');
+      }
+      else {
+        this.client.emit(
+          'request',
+          { method, data },
+          this.timeoutCallback((err, response) => {
+            if (err) {
+              reject(err)
+            }
+            else {
+              resolve(response)
+            }
+          })
+        )
+      }
+    })
+  }
+}
+
+module.exports = { SoupClient }
---
+++ nodebc/playlistbot.js
@@ -0,0 +1,204 @@
+const socketio = require('socket.io-client')
+const faker = require('faker')
+const execa = require('execa')
+const { SoupClient } = require('./lib/soupclient')
+
+// console.log(SoupClient)
+
+async function main() {
+
+  const startYoutubeGst = async function (url) {
+    await soupClient.startProducers()
+    const {
+      videoTransportIp,
+      videoTransportPort,
+      videoTransportRtcpPort,
+      videoPt,
+      videoSSRC,
+      audioTransportIp,
+      audioTransportPort,
+      audioTransportRtcpPort,
+      audioPt,
+      audioSSRC,
+    } = soupClient.transportOpts
+
+    const formatcmd = [
+      '--list-formats',
+      url 
+    ]
+
+
+    console.log('getting formats')
+    const formatres = await execa('/usr/bin/youtube-dl', formatcmd)
+    console.log('got it', formatres.stdout)
+    lines = formatres.stdout.split(/\n/)
+    let format = null
+    let isVideo = false
+    lines.forEach((line) => {
+      if (line.match(/^format/)) { return }
+      parts = line.split(/\s+/)
+      const f = parts[0]
+      switch (f) {
+        case 'http_mp3_128':
+        case 'mp3':
+        case 'mp3-128':
+          format = f
+          isVideo = false
+          break
+        default:
+          break
+      }
+    })
+    // attempt a youtube donwload if nothing matched
+
+    if (!format) {
+      format = "18"
+      isVideo = true
+    }
+
+    const ytdlcmd = [
+      `-f ${format}`,
+      '-4',
+      '--get-url',
+      url 
+    ]
+
+    
+    console.log("chosen format", format)
+
+    console.log('getting yturl')
+    const ytres = await execa('/usr/bin/youtube-dl', ytdlcmd)
+    console.log('got it', ytres.stdout)
+    const yturl = ytres.stdout.replace(/([;'"`#$&*?<>\\])/g, "\\$1")
+
+    let command
+
+    if (isVideo) {
+      // ! avdec_aac ! audioconvert \
+      command = `/usr/bin/gst-launch-1.0 \
+        rtpbin name=rtpbin latency=1000 rtp-profile=avpf \
+        souphttpsrc is-live=true location=${yturl} \
+         ! qtdemux name=demux \
+        demux.video_0 \
+         ! queue \
+         ! decodebin \
+         ! videoconvert \
+         ! vp8enc target-bitrate=1000000 deadline=1 cpu-used=4 \
+         ! rtpvp8pay pt=${videoPt} ssrc=${videoSSRC} picture-id-mode=2 \
+         ! rtpbin.send_rtp_sink_0 \
+        rtpbin.send_rtp_src_0 ! udpsink host=${videoTransportIp} port=${videoTransportPort} \
+        rtpbin.send_rtcp_src_0 ! udpsink host=${videoTransportIp} port=${videoTransportRtcpPort} sync=false async=false \
+        demux.audio_0 \
+         ! queue \
+         ! decodebin \
+         ! audioresample \
+         ! audioconvert \
+         ! opusenc \
+         ! rtpopuspay pt=${audioPt} ssrc=${audioSSRC} \
+         ! rtpbin.send_rtp_sink_1 \
+        rtpbin.send_rtp_src_1 ! udpsink host=${audioTransportIp} port=${audioTransportPort} \
+        rtpbin.send_rtcp_src_1 ! udpsink host=${audioTransportIp} port=${audioTransportRtcpPort} sync=false async=false \
+      `
+    } else {
+      command = `/usr/bin/gst-launch-1.0 \
+        rtpbin name=rtpbin latency=1000 rtp-profile=avpf \
+        souphttpsrc is-live=true location=${yturl} \
+         ! queue ! decodebin ! audioconvert \
+         ! audioresample \
+         ! audioconvert \
+         ! opusenc \
+         ! rtpopuspay pt=${audioPt} ssrc=${audioSSRC} \
+         ! rtpbin.send_rtp_sink_1 \
+        rtpbin.send_rtp_src_1 ! udpsink host=${audioTransportIp} port=${audioTransportPort} \
+        rtpbin.send_rtcp_src_1 ! udpsink host=${audioTransportIp} port=${audioTransportRtcpPort} sync=false async=false \
+      `
+    }
+    console.log(command)
+    await soupClient.execGstCommand(command)
+  }
+
+  const handleCommand = async function(command) {
+    if (!soupClient.joined) {
+      console.log('Not joined')
+      return
+    }
+
+    // play command
+    if (matched = command.match(/^play (.*)/)) {
+      console.log("play command received")
+      const url = matched[1]
+      // if (!url.match(/^(http(s)??\:\/\/)?(www\.)?((youtube\.com\/watch\?v=)|(youtu.be\/))([a-zA-Z0-9\-_])+/)) {
+      //  console.log(`play url did not match: ${url}`)
+      //  await sendChatMessage(`This does not look like a youtube URL to me: ${url}. I will not play it.`)
+      //  return
+      // }
+
+      if (soupClient.playing === true) {
+        console.log("stopping current track")
+        await soupClient.sendChatMessage(`Stopping the currently running track ${soupClient.url}.`)
+        await soupClient.stopCurrentTrack()
+      }
+      await soupClient.sendChatMessage(`Attempt on youtube video ${url}`)
+      await soupClient.startYoutubeGst(url)
+      soupClient.url = url
+      await soupClient.sendChatMessage(`Playing youtube video ${url}`)
+    }
+
+    // stop command
+    else if (matched = command.match(/^stop/)) {
+      // console.log(soupClient)
+      if (soupClient.playing === true) {
+        await soupClient.sendChatMessage(`I am stopping the currently running track ${state.url}.`)
+        console.log("stopping current track")
+        await soupClient.stopCurrentTrack()
+      }
+    }
+
+    // unknown
+    else {
+      console.log(`ignoring ${command}`)
+    }
+  }
+
+  ////////////////////////////////////////
+
+  const roomId = 'miniclub'
+  const displayName = "PlaylistBot"
+  const url = 'https://soup.jetpack.cl:5443'
+
+  const soupClient = new SoupClient(url, roomId, displayName)
+
+  soupClient.on('chatMessage', async function (data) {
+    try {
+      const { peerId, chatMessage } = data
+      console.log("got chat message: ", data)
+      if (chatMessage.type === 'message') {
+        console.log("got chat message of type 'text': ", chatMessage.text)
+        await handleCommand(chatMessage.text)
+      }
+    }
+    catch (error) {
+      console.error('error on chatMessage failed: ', error);
+      await sendChatMessage(`Shit: Error ${JSON.stringify(error)}`)
+    }
+  })
+
+
+  soupClient.on('disconnect', function () {
+    console.log("disconnect")
+  })
+
+  await soupClient.connect()
+
+  // keep running
+  await new Promise(function () { })
+  console.log('This text will never be printed')
+}
+
+(async () => {
+  try {
+    await main()
+  } catch(e) {
+    console.log(e)
+  }
+})()
