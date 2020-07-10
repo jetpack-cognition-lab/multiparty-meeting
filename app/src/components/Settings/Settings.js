1fc1dbf10bbb12d0747db71a0439f49ddd328fee
Oswald Berthold <oswald.berthold@gmail.com>
2020-07-10 17:23:51 +0200

    add 15, 20, 30 as options for num visible videos

--- app/src/components/Settings/Settings.js
+++ app/src/components/Settings/Settings.js
@@ -312,7 +312,7 @@ const Settings = ({
 								autoWidth
 								className={classes.selectEmpty}
 							>
-								{ [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ].map((lastN) =>
+								{ [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30 ].map((lastN) =>
 								{
 									return (
 										<MenuItem key={lastN} value={lastN}>
